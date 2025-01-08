e21dc503e4b3c5a165ec98759de13cb8f44b6b1c
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2025-01-08 08:09:22 +0100

    updated manifest

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,7 +42,7 @@ class _PaymentPageState extends State<PaymentPage> {
             gatewayType: GatewayType
                 .paystack, // toggle between GatewayType.paystack and GatewayType.flutterwave
             checkoutUrl:
-                "https://checkout.paystack.com/grz0f3mpmydefr9", // eg. https://checkout.paystack.com/xlt21ud3wz0985r
+                "https://checkout.paystack.com/o1dgxif7zbk6he2", // eg. https://checkout.paystack.com/xlt21ud3wz0985r
             onSuccess: () {
               // Is called when payment succeeds
               Navigator.pushReplacementNamed(context, '/success');
